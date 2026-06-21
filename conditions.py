8c7e413a549b6626fa8b6ac936dd25870b87a6ab
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-21 14:57:22 +0300

    finish the step 2

--- conditions.py
+++ conditions.py
@@ -1,5 +1,14 @@
+# part 1
+# step 1
 age = int(input("enter your age "))
 if age >= 18:
     print("Can enter")
 else:
     print("Cannot enter")
+# step 2
+temperature = 30
+if temperature > 37.5:
+    print("High temperature")
+else:
+    print("Normal temperature")
+# step 3
